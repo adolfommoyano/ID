7d69efd004f80ad1a1fad193cd76b19e5a5473da
adolfommoyano <98586598+adolfommoyano@users.noreply.github.com>
2022-11-18 17:28:45 +0100

    Update genesis_start.sh

--- genesis_start.sh
+++ genesis_start.sh
@@ -8,8 +8,8 @@ fi
 sed -i "s/IDBOTIC_EOSIO_PUBLIC_KEY/$IDBOTIC_EOSIO_PUBLIC_KEY/" genesis.json
 
 nodeos \
---genesis-json $DATADIR"/../../genesis.json" \
---signature-provider $IDBOTIC_EOSIO_PUBLIC_KEY=KEY:$IDBOTIC_EOSIO_PRIVATE_KEY\
+--genesis-json genesis.json \
+--signature-provider $IDBOTIC_EOSIO_PUBLIC_KEY=KEY:$IDBOTIC_EOSIO_PRIVATE_KEY \
 --plugin eosio::producer_plugin \
 --plugin eosio::chain_api_plugin \
 --plugin eosio::http_plugin \
@@ -27,5 +27,4 @@ nodeos \
 --verbose-http-errors \
 --enable-stale-production \
 >> $DATADIR"/nodeos.log" 2>&1 & \
-echo $! > $DATADIR"/eosd.pid \
-
+echo $! > $DATADIR"/eosd.pid 
